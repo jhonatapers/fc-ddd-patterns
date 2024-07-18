3014c16647f38aad9e568d9618057ce6ab78e543
Jhonata Peres <jhonatapers@gmail.com>
2024-07-18 13:05:52 -0300

    add miss import

--- src/domain/customer/service/cutomer.service.ts
+++ src/domain/customer/service/cutomer.service.ts
@@ -1,7 +1,9 @@
 import EventDispatcherInterface from "../../@shared/event/event-dispatcher.interface";
 import Customer from "../entity/customer";
+import CustomerAddressChangedEvent from "../event/customer-address-changed.event";
 import CustomerCreatedEvent from "../event/customer-created.event";
 import CustomerRepositoryInterface from "../repository/customer-repository.interface";
+import Address from "../value-object/address";
 
 export default class CustomerService {
 
