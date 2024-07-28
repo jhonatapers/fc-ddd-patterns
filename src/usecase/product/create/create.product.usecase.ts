95c403b0f94c2445d2adec89af475ff6c4c30d44
jhonatapers <jhonatapers@gmail.com>
2024-07-28 13:52:25 -0300

    [fix] file name

--- src/usecase/product/create/create.product.usecase.ts
+++ src/usecase/product/create/create.product.usecase.ts
@@ -1,7 +1,7 @@
 import Product from "../../../domain/product/entity/product";
 import { v4 as uuid } from "uuid";
 import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
-import { InputCreateProductDto, OutputCreateProductDto } from "./create.product.dt";
+import { InputCreateProductDto, OutputCreateProductDto } from "./create.product.dto";
 
 export default class CreateProductUseCase {
 
