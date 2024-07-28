23ce43272bef8409210ad95211af32a98844c428
jhonatapers <jhonatapers@gmail.com>
2024-07-28 16:48:35 -0300

    customer list rest api

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -52,5 +52,52 @@ describe("E2E test for customer", () => {
         expect(response.status).toBe(500);
     });
 
+    it("should list all customers", async () => {
+
+        const response1 = await request(app)
+            .post("/customers")
+            .send({
+                name: "John",
+                address: {
+                    street: "Street",
+                    city: "City",
+                    number: 123,
+                    zip: "12345"
+                }
+            });
+
+        expect(response1.status).toBe(200);
+
+        const response2 = await request(app)
+            .post("/customers")
+            .send({
+                name: "Jane",
+                address: {
+                    street: "Street 2",
+                    city: "City 2",
+                    number: 1234,
+                    zip: "12346"
+                }
+            });
+
+        expect(response2.status).toBe(200);
+
+        const listResponse = await request(app)
+            .get("/customers")
+            .send();
+
+        expect(listResponse.status).toBe(200);
+        expect(listResponse.body.customers.length).toBe(2);
+        const customer1 = listResponse.body.customers[0];
+
+        expect(customer1.name).toBe("John")
+        expect(customer1.address.street).toBe("Street")
+
+        const customer2 = listResponse.body.customers[1];
+        expect(customer2.name).toBe("Jane")
+        expect(customer2.address.street).toBe("Street 2")
+
+    });
+
 
 });
