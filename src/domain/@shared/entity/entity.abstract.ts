c0d7dcc2846de911616082b16f3ca95bec230206
jhonatapers <jhonatapers@gmail.com>
2024-07-28 19:23:48 -0300

    customer validator with yup

--- src/domain/@shared/entity/entity.abstract.ts
+++ src/domain/@shared/entity/entity.abstract.ts
@@ -2,13 +2,18 @@ import Notification from "../notification/notification";
 
 export default abstract class Entity {
     protected _id: string;
-    protected notification: Notification;
+    private _notification: Notification;
+
 
     constructor() {
-        this.notification = new Notification();
+        this._notification = new Notification();
     }
 
     get id(): string {
         return this._id;
+    }
+
+    get notification(): Notification {
+        return this._notification;
     }
 }
