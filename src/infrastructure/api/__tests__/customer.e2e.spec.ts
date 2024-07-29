14e3c7fe0d2504fed3b001a8318df8ebe5e3ca50
jhonatapers <jhonatapers@gmail.com>
2024-07-28 21:36:30 -0300

    customer presenter

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -97,6 +97,14 @@ describe("E2E test for customer", () => {
         expect(customer2.name).toBe("Jane")
         expect(customer2.address.street).toBe("Street 2")
 
+        const listResponseXml = await request(app)
+            .get('/customers')
+            .set('Accept', "application/xml")
+            .send();
+
+        expect(listResponseXml.status).toBe(200);
+        expect(listResponseXml.text).toContain('<?xml version="1.0" encoding="UTF-8"?>')
+
     });
 
 
